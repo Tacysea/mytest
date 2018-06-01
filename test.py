462ded69d1853e2c704a1038147e7306b6bd80c4
xwp <xwp_fullstack@163.com>
2018-06-01 14:39:54 +0800

    写出项目初始框架

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-print('hello world!!!')
+from flask import Flask
+from flask_script import Manager
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+manager = Manager(app)
+
+@app.route('/')
+def index():
+    return 'hello world'
+
+if __name__ == '__main__':
+    manager.run()
